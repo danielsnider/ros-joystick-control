eb257ffed4cb4d2052a3e746c8d26779ff958f9b
danielsnider <danielsnider12@gmail.com>
2017-02-17 11:36:41 -0800

    Support for JointTrajectory ROS messages

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -3,48 +3,67 @@
 //
 
 #include <Arduino.h>
-#include "Servo.h"
 #include "ros.h"
-#include "geometry_msgs/Twist.h"
 #include "sensor_msgs/JointState.h"
 #include <control_msgs/JointTrajectoryActionGoal.h>
-#include <control_msgs/JointTrajectoryActionGoal.h>
 #include <trajectory_msgs/JointTrajectory.h>
+#include <trajectory_msgs/JointTrajectoryPoint.h>
 
 ros::NodeHandle nh;
-Servo theServo;
 
-const int deadZoneThreshold = 93;
 
-int rawSpeedFromX(float x) {
-    if (x < 0) {
-        return (int) (deadZoneThreshold - (-x * deadZoneThreshold));
-    }
-    else {
-        return ((int) (93 + (x * deadZoneThreshold)));
-    }
+// void jt_callback(const trajectory_msgs::JointTrajectoryConstPtr &msg) {
+void jt_callback(const trajectory_msgs::JointTrajectory &msg) {
+  nh.loginfo("msg received");
+
+  // Print msg.points_length
+  char log_msg[80];
+  sprintf(log_msg,"msg.points = %i",msg.points_length);
+  nh.loginfo(log_msg);
+
+  //Print msg.joint_names[0]
+  sprintf(log_msg,"msg.joint_names[0] = %s",msg.joint_names[0]);
+  nh.loginfo(log_msg);
+
+  trajectory_msgs::JointTrajectoryPoint *points = msg.points;
+
+
+  // Print msg.points[0].positions_length
+  sprintf(log_msg,"msg.points[0].positions_length = %d",points[0].positions_length);
+  nh.loginfo(log_msg);
+
+  // Print points.positions[0]
+  char result[8]; // Buffer big enough for 7-character float
+  dtostrf(points[0].positions[0], 6, 2, result); // Leave room for too large numbers!
+  sprintf(log_msg,"points[0].positions[0] =%s", result);
+  nh.loginfo(log_msg);
+
+  // Print points.positions[1]
+  dtostrf(points[0].positions[1], 6, 2, result); // Leave room for too large numbers!
+  sprintf(log_msg,"points[0].positions[1] =%s", result);
+  nh.loginfo(log_msg);
+
+  // Print points.positions[2]
+  dtostrf(points[0].positions[2], 6, 2, result); // Leave room for too large numbers!
+  sprintf(log_msg,"points[0].positions[2] =%s", result);
+  nh.loginfo(log_msg);
+
+  // Print points.positions[3]
+  dtostrf(points[0].positions[3], 6, 2, result); // Leave room for too large numbers!
+  sprintf(log_msg,"points[0].positions[3] =%s", result);
+  nh.loginfo(log_msg);
+
 }
 
-void callback(const geometry_msgs::Twist& msg) {
-    int speed = rawSpeedFromX(msg.linear.x);
-    theServo.write(speed);
-}
-
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", callback);
+ros::Subscriber<trajectory_msgs::JointTrajectory> jt_sub_("arm_controller/command", jt_callback);
 
 void setup() {
-    theServo.attach(9);
     nh.initNode();
-    nh.subscribe(sub);
-    theServo.write(93);
+    nh.subscribe(jt_sub_);
+    nh.spinOnce();
 }
 
 void loop() {
     nh.spinOnce();
-    //nh.logdebug("Debug Statement");
-    //nh.loginfo("Program info");
-    //nh.logwarn("Warnings.");
-    //nh.logerror("Errors..");
-    //nh.logfatal("Fatalities!");
-
+    // nh.loginfo("Program loop");
 }
--- notes.md
+++ notes.md
@@ -8,4 +8,27 @@ sudo service udev restart
 pio run -t upload
 cd ~/ros-joystick-control/
 source devel/setup.bash
+
+
+# start a stream of commands
+rostopic  pub -r10 /arm_controller/command trajectory_msgs/JointTrajectory '{joint_names:  ["hip", "shoulder", "elbow",  "wrist"], points: [{positions:  [0.1, -0.5, 0.5,  0.75],  tim
+e_from_start:  [1.0, 0.0]}]}'
+
+# monitor the commands
+rostopic echo /arm_controller/command
+
 roslaunch launcher.launch
+
+# output should be
+[INFO] [1487354908.155966, 7398.368000]: msg received
+[INFO] [1487354908.162019, 7398.370000]: msg.points = 1
+[INFO] [1487354908.180214, 7398.373000]: msg.joint_names[0] = hip
+[INFO] [1487354908.194747, 7398.377000]: msg.points[0].positions_length = 4
+[INFO] [1487354908.206841, 7398.382000]: points[0].positions[0] =  0.10
+[INFO] [1487354908.220732, 7398.391000]: points[0].positions[1] = -0.50
+[INFO] [1487354908.233679, 7398.396000]: points[0].positions[2] =  0.50
+[INFO] [1487354908.237420, 7398.400000]: points[0].positions[3] =  0.75
+
+# edit code and relaunch
+
+pio run -t upload && roslaunch ../launcher.launch
